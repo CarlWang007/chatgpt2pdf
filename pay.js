0d9529992a0792bcd49ba33279d1f58d3c1c17c1
carl00777 <313633588@qq.com>
2023-05-28 11:26:10 +0800

    update 0528

---
+++ pay.js
@@ -0,0 +1,11 @@
+const payNowMBtn = document.getElementById('payNowM');    
+payNowMBtn.addEventListener('click', async () => {    
+  const newWindow = window.open('https://chatgpt2pdf.lemonsqueezy.com/checkout/buy/e1233007-3830-482a-9ce7-f26df0a8dbab');
+  
+}); 
+
+const payNowYBtn = document.getElementById('payNowY');    
+payNowYBtn.addEventListener('click', async () => {    
+  const newWindow = window.open('https://chatgpt2pdf.lemonsqueezy.com/checkout/buy/3c486697-a8b0-450e-9413-15a0456408e1');
+  
+}); 
